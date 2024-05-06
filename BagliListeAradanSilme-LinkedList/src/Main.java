778eb7bb54d5f9305b8cbd2603c8ac79f6bae237
yusufaytasss <aytasyusuf642@gmail.com>
2024-05-06 13:35:41 +0300

    Linked List Aradan Eleman Silme

---
+++ BagliListeAradanSilme-LinkedList/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        ListStructure liste = new ListStructure();
+        liste.basaEkle(10);
+        liste.basaEkle(20);
+        liste.basaEkle(30);
+        liste.basaEkle(40);
+        liste.basaEkle(50);
+        liste.basaEkle(60);
+
+        liste.BastanSil();
+        liste.SondanSil();
+        liste.AradanSil(3);
+        liste.listele();
+    }
+}
